15993cc656b93b8e157032d94d273676fcdf3025
ghoshonkar5 <139379959+ghoshonkar5@users.noreply.github.com>
2025-11-01 20:29:34 +0530

    fix: corrected Particles import/export issue

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -1,4 +1,4 @@
-import { Timeline } from "../components/Timeline";
+import { Timeline }  from "../components/Timeline";
 import { experiences } from "../constants";
 const Experiences = () => {
   return (
